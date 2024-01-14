49eea87de88354da8c39ee25919880307daefa25
Hizhniy <hizhniy@gmail.com>
2024-01-14 19:04:46 +0300

    Добавьте файлы проекта.

---
+++ HomeApp/HomeApp/Pages/LoadingPage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace HomeApp.Pages
+{
+    class LoadingPage : ContentPage
+    {
+        public LoadingPage()
+        {
+            // Объявим новый текстовый элемент
+            Label header = new Label() { Text = $"Запуск вашего первого приложения{Environment.NewLine} на Xamarin..." };
+
+            // Здесь можно сразу установить стили и шрифт
+            header.Opacity = 0;
+            header.HorizontalTextAlignment = TextAlignment.Center;
+            header.VerticalTextAlignment = TextAlignment.Center;
+            header.FontSize = 21;
+            // Можем даже задать анимацию
+            header.FadeTo(1, 3000);
+
+            // Инициализация свойства Content новым элементом.
+            Content = header;
+        }
+    }
+
+    class SpanPage : ContentPage
+    {
+        public SpanPage()
+        {
+            // Инициализация объекта
+            Label label = new Label()
+            {
+                FontSize = 16,
+                Padding = new Thickness(30, 24, 30, 0),
+                FormattedText = new FormattedString()
+                {
+                    Spans =
+               {
+                   new Span() { Text = "Learn more at " },
+                   new Span() { Text = "https://aka.ms/xamarin-quickstart", FontAttributes = FontAttributes.Bold }
+               }
+                }
+            };
+
+            // Добавление на страницу
+            Content = label;
+        }
+    }
+}
---
+++ HomeApp/HomeApp/Pages/LoginPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HomeApp.Pages
+{
+    public partial class LoginPage : ContentPage
+    {
+        // Константа для текста кнопки
+        public const string BUTTON_TEXT = "Войти";
+        // Переменная счетчика
+        public static int loginCouner = 0;
+
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// По клику обрабатываем счётчик и выводим разные сообщения
+        /// </summary>
+        private void Login_Click(object sender, EventArgs e)
+        {
+            if (loginCouner == 0)
+            {
+                loginButton.Text = $"Выполняется вход..";
+            }
+            else if (loginCouner > 5)
+            {
+                loginButton.IsEnabled = false;
+
+                // Получаем последний дочерний элемент, используя свойство Children, затем выполняем распаковку
+                var infoMessage = (Label)stackLayout.Children.Last();
+                // Задаем текст элемента
+                infoMessage.Text = "Слишком много попыток! Попробуйте позже";
+            }
+            else
+            {
+                loginButton.Text = $"Выполняется вход...   Попыток входа: {loginCouner}";
+            }
+
+            loginCouner += 1;
+        }
+    }
+}
