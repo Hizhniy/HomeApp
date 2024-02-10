c2cae1671c6fd2d7285b788a311f5d18113d6eb0
Hizhniy <hizhniy@gmail.com>
2024-02-10 20:15:37 +0300

    40.5 Full

--- HomeApp/HomeApp/App.xaml.cs
+++ HomeApp/HomeApp/App.xaml.cs
@@ -1,41 +1,50 @@
 ﻿using System;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+using System.IO;
 using HomeApp.Pages;
+using HomeApp.Data;
+using AutoMapper;
 
 namespace HomeApp
 {
     public partial class App : Application
     {
+        // Инициализация репозитория
+        public static HomeDeviceRepository HomeDevices = new HomeDeviceRepository(
+            Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                $"homedevices.db")
+            );
+
+        public static IMapper Mapper { get; set; }
 
         public App()
         {
+            Mapper = CreateMapper();
+
             // инициализация интерфейса
             InitializeComponent();
             // Инициализация главного экрана и стека навигации
             MainPage = new NavigationPage(new LoginPage());
+        }
 
-            //MainPage = new DevicesPage();
-            //MainPage = new DeviceControlPage();
-            //MainPage = new NewDevicePage();
-            //MainPage = new CsharpPaddingPage();
-            //MainPage = new PaddingPage();
-            //MainPage = new GridPage();
-            //MainPage = new AboutPage();
-            //MainPage = new ClimatePage();
-            //MainPage = new LoadingPage();
-            //MainPage = new MainPage();
-            //MainPage = new LoginPage();
-            //MainPage = new RegisterPage();
-            //MainPage = new ProfilePage();
-            //MainPage = new WebPage();
-            //MainPage = new AlarmPage();
-            //MainPage = new BindingPage();
-            //MainPage = new BindingModePage();
-        }    
-
-        protected override void OnStart()
+        /// <summary>
+        /// Создание Автомаппера для преобразования сущностей
+        /// </summary>
+        public static IMapper CreateMapper()
         {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Data.Tables.HomeDevice, Models.HomeDevice>();
+                cfg.CreateMap<Models.HomeDevice, Data.Tables.HomeDevice>();
+            });
+
+            return config.CreateMapper();
+        }
+
+        protected async override void OnStart()
+        {
+            await HomeDevices.InitDatabase();
         }
 
         protected override void OnSleep()
--- HomeApp/HomeApp/Pages/DeviceListPage.xaml.cs
+++ HomeApp/HomeApp/Pages/DeviceListPage.xaml.cs
@@ -1,21 +1,17 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Linq;
+using System.Collections.ObjectModel;
 using Xamarin.Forms;
 using HomeApp.Models;
-using System.Linq;
-using System;
-using System.Collections.ObjectModel;
 
 namespace HomeApp.Pages
 {
     public partial class DeviceListPage : ContentPage
     {
-
-        public List<HomeDevice> Devices { get; set; } = new List<HomeDevice>();
-
         /// <summary>
         /// Группируемая коллекция
         /// </summary>
-        public ObservableCollection<Group<string, HomeDevice>> DeviceGroups { get; set; } = new ObservableCollection<Group<string, HomeDevice>>();
+        public ObservableCollection<Group<string, HomeDevice>> DeviceGroups { get; set; }
 
         /// <summary>
         /// Ссылка на выбранный объект
@@ -25,20 +21,23 @@ namespace HomeApp.Pages
         public DeviceListPage()
         {
             InitializeComponent();
+        }
 
-            // Первоначальные данные сохраним в обычном листе
-            var initialList = new List<HomeDevice>();
-            initialList.Add(new HomeDevice("Чайник", "Chainik.png", "LG, объем 2л.", "Кухня") { Id = new Guid("735a3848-dad1-40f6-8fb7-36d2da16f1f3") });
-            initialList.Add(new HomeDevice("Стиральная машина", "StiralnayaMashina.png", description: "BOSCH", "Ванная") { Id = new Guid("cca22a07-5593-4485-b21d-ed7aba4ac815") });
-            initialList.Add(new HomeDevice("Посудомоечная машина", "PosudomoechnayaMashina.png", "Gorenje", "Кухня") { Id = new Guid("eba7c7eb-941b-4b1d-a179-21fa87a783ac") });
-            initialList.Add(new HomeDevice("Мультиварка", "Multivarka.png", "Philips", "Кухня") { Id = new Guid("7e19330d-23f2-4fe7-b8b7-14d3c8163e73") });
+        protected async override void OnAppearing()
+        {
+            // Загрузка данных из базы
+            var devicesFromDb = await App.HomeDevices.GetHomeDevices();
+            // Мапим сущности БД в сущности бизнес-логики
+            var deviceList = App.Mapper.Map<Models.HomeDevice[]>(devicesFromDb);
 
             // Сгруппируем по комнатам
-            var devicesByRooms = initialList.GroupBy(d => d.Room).Select(g => new Group<string, HomeDevice>(g.Key, g));
+            var devicesByRooms = deviceList.GroupBy(d => d.Room).Select(g => new Group<string, HomeDevice>(g.Key, g));
 
             // Сохраним
             DeviceGroups = new ObservableCollection<Group<string, HomeDevice>>(devicesByRooms);
             BindingContext = this;
+
+            base.OnAppearing();
         }
 
         /// <summary>
@@ -49,7 +48,7 @@ namespace HomeApp.Pages
             // распаковка модели из объекта
             var tappedDevice = (HomeDevice)e.Item;
             // уведомление
-            DisplayAlert("Нажатие", $"Вы нажали на элемент {tappedDevice.Name}", "OK");
+            DisplayAlert("Нажатие", $"Вы нажали на элемент {tappedDevice.Name}", "OK"); ; ;
         }
 
         /// <summary>
@@ -61,42 +60,6 @@ namespace HomeApp.Pages
             SelectedDevice = (HomeDevice)e.SelectedItem;
         }
 
-        /// <summary>
-        /// Обработчик добавления нового устройства
-        /// </summary>
-        private async void AddDevice(object sender, EventArgs e)
-        {
-            // Запрос и валидация имени устройства
-            var newDeviceName = await DisplayPromptAsync("Новое устройство", "Введите имя устройства", "Продолжить", "Отмена");
-            if (Devices.Any(d => d.Name.CompareTo(newDeviceName.Trim()) == 0))
-            {
-                await DisplayAlert("Ошибка", $"Устройство '{newDeviceName}' уже существует", "ОК");
-                return;
-            }
-
-            // Запрос описания устройства
-            var newDeviceDescription = await DisplayPromptAsync(newDeviceName, "Добавьте краткое описание устройства", "Сохранить", "Отмена");
-
-            // Добавление устройства и уведомление пользователя
-            Devices.Add(new HomeDevice(newDeviceName, description: newDeviceDescription));
-            await DisplayAlert(null, $"Устройство '{newDeviceName}' успешно добавлено", "ОК");
-        }
-
-        /// <summary>
-        /// Обработчик удаления устройства
-        /// </summary>
-        private async void RemoveDevice(object sender, EventArgs e)
-        {
-            // Получаем и "распаковываем" выбранный элемент
-            var deviceToRemove = deviceList.SelectedItem as HomeDevice;
-            if (deviceToRemove != null)
-            {
-                // Удаляем
-                Devices.Remove(deviceToRemove);
-                // Уведомляем пользователя
-                await DisplayAlert(null, $"Устройство '{deviceToRemove.Name}' удалено", "ОК");
-            }
-        }
         private async void LogoutButton_Clicked(object sender, EventArgs e)
         {
             // Возврат на первую страницу стека навигации (корневую страницу приложения) - экран логина
@@ -106,7 +69,7 @@ namespace HomeApp.Pages
         private async void NewDeviceButton_Clicked(object sender, EventArgs e)
         {
             // Переход на следующую страницу - страницу нового устройства (и помещение её в стек навигации)
-            await Navigation.PushAsync(new NewDevicePage());
+            await Navigation.PushAsync(new DevicePage("Новое устройство"));
         }
 
         private async void EditDeviceButton_Clicked(object sender, EventArgs e)
@@ -125,6 +88,19 @@ namespace HomeApp.Pages
         private async void UserProfileButton_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new ProfilePage());
+        }
+
+        private async void DeleteButton_Clicked(object sender, EventArgs e)
+        {
+            // Получаем сущность базы данных, которую следует удалить (мапим из внутренней сущности, представляющей выбранное устройство)
+            var deviceToDelete = App.Mapper.Map<Data.Tables.HomeDevice>(SelectedDevice);
+            // Удаляем сущность из бд
+            await App.HomeDevices.DeleteHomeDevice(deviceToDelete);
+
+            // Обновляем интерфейс
+            var grp = DeviceGroups.FirstOrDefault(g => g.Name == SelectedDevice.Room);
+            var deviceToRemove = grp.FirstOrDefault(d => d.Id == deviceToDelete.Id);
+            grp.Remove(deviceToRemove);
         }
     }
 }
